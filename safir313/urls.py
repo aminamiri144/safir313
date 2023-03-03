74d15e59227defdbb02de6af3b56d03985bde112
amiri <aminamiri144@gmail.com>
2023-03-03 13:11:13 +0330

    add Login page

--- safir313/urls.py
+++ safir313/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-
+from .views import UserLoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', UserLoginView.as_view(), name='login'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.ASSETS_URL, document_root=settings.ASSETS_ROOT)
